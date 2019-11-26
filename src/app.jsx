b0da32f72019ed4262cec9f078ce88105a221a1d
wangjingming <wangjingming2@outlook.com>
2019-11-26 20:52:51 +0800

    add app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var appWrapper = document.getElementById('app');
+
+function App(props) {
+    return <div><h1>hello world</h1></div>
+}
+
+ReactDOM.render(<App />, appWrapper);
